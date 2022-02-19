1dd0a46b0a45e2ef0b9f3304fb9966abf99a750d
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-02-19 14:47:33 +0200

    Update WKWebView public extension (#8)
    * Add WKWebView public extension func
    
    * Add comment

--- WebKitUserAgent/Sources/Extensions/WKWebView+Extensions.swift
+++ WebKitUserAgent/Sources/Extensions/WKWebView+Extensions.swift
@@ -37,3 +37,10 @@ extension WKWebView {
         }
     }
 }
+
+public extension WKWebView {
+    /// Fetching `User Agent` through `WKWebView`.
+    func fetchUserAgent(completion: @escaping (Result<String, Error>) -> Void) {
+        WKUserAgent.fetch(fromWebView: self, completion: completion)
+    }
+}
