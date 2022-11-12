024c7439ec31c3d77423908e443503f88c3fa063
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-11-12 18:17:03 +0200

    Update license

--- WebKitUserAgent/Sources/Extensions/WKWebViewConfiguration+Extensions.swift
+++ WebKitUserAgent/Sources/Extensions/WKWebViewConfiguration+Extensions.swift
@@ -1,26 +1,9 @@
 //
-//  Copyright (c) 2022 Dmytrii Golovanov
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
 //  WKWebViewConfiguration+Extensions.swift
+//  WebKitUserAgent
+//
 //  Created by Dmytrii Golovanov on 03.12.2021.
+//  Copyright © 2022 Dmytrii Golovanov. All rights reserved.
 //
 
 import Foundation
@@ -34,4 +17,22 @@ extension WKWebViewConfiguration {
             return preferences.javaScriptEnabled
         }
     }
+    
+    convenience init(
+        applicationName: String,
+        rewriteDefaultApplicationName: Bool
+    ) {
+        self.init()
+        var targetApplicationName: String = ""
+        if rewriteDefaultApplicationName == false,
+           let defaultApplicationName = self.applicationNameForUserAgent {
+            targetApplicationName += defaultApplicationName
+        }
+        if targetApplicationName.isEmpty == false {
+            targetApplicationName += " "
+        }
+        targetApplicationName += applicationName
+        self.applicationNameForUserAgent = targetApplicationName
+    }
+}
 }
